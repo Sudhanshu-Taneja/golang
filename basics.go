cf2f8390ee0497c46bccf451b93cc062520f6044
Sudhanshu <pstanej1.msp.amadeus.net>
2025-07-08 13:48:27 +0530

    Adding struct

--- basics.go
+++ basics.go
@@ -2,9 +2,22 @@ package main // Package decoration which builds the file into executable go prog
 
 import "fmt" // Importing fmt package from standard library in order to use it.
 
-func add(x, y int) int { // The type of variables can declared together in synchronized manner.
+func calc(x, y int) (int, int, int) { // The type of variables can declared together in synchronized manner.
 	// This function takes two integers x and y, adds them, and returns the result.
-	return x + y
+	return (x + y), (x - y), (x * y) // Return the sum and difference of x and y.
+}
+
+type car struct {
+	brand      string // Field brand of type string.
+	model      string // Field model of type string.
+	year       int    // Field year of type int.
+	Frontwheel Wheel  // Field Frontwheel of type Wheel.
+	Backwheel  Wheel  // Field Backwheel of type Wheel.
+}
+
+type Wheel struct {
+	Radius   int    // Field size of type int.
+	Material string // Field type of type string.
 }
 
 func main() {
@@ -12,6 +25,24 @@ func main() {
 	// Print the value of num to the console.
 	fmt.Println(num)
 	// Print a string to the console.
-	res := add(5, 7)                // Call the add function with arguments 5 and 7, and assign the result to res.
-	fmt.Println("The sum is:", res) // Print the result of the addition to the console.
+	add, sub, _ := calc(10, 7)             // Call the add function with arguments 10 and 7, and assign the result to add and sub but ignore the multipication output.
+	fmt.Println("The sum is:", add)        // Print the result of the addition to the console.
+	fmt.Println("The difference is:", sub) // Print the result of the subtraction to the console.
+
+	mycar := car{ // Create a new car instance with specified fields.
+		brand: "Toyota",  // Assign the brand field to "Toyota".
+		model: "Corolla", // Assign the model field to "Corolla".
+		year:  2020,      // Assign the year field to 2020.
+		Frontwheel: Wheel{ // Assign the Frontwheel field to a new Wheel instance.
+			Radius:   15,       // Assign the Radius field of Frontwheel to 15.
+			Material: "Rubber", // Assign the Material field of Frontwheel to "Rubber".
+		},
+		Backwheel: Wheel{ // Assign the Backwheel field to a new Wheel instance.
+			Radius:   15,       // Assign the Radius field of Backwheel to 15.
+			Material: "Rubber", // Assign the Material field of Backwheel to "Rubber".
+		},
+	}
+	fmt.Println("Car details:")        // Print a header for car details.
+	fmt.Println("Brand:", mycar.brand) // Print the brand of the car.
+	fmt.Println("Model:", mycar.model) // Print the model of the car.
 }
