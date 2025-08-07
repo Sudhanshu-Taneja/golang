21d51d2b669b94362f57b32466d9550b2341fc4c
Sudhanshu <sudhanshutaneja001@gmail.com>
2025-08-07 23:28:21 +0530

    Setting up postgres database

---
+++ rss-project/handler_user.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Sudhanshu-Taneja/rsagg/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		errorResponse(w, 400, fmt.Sprintf("Error parsing json: %v", err))
+		return
+	}
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+
+	if err != nil {
+		errorResponse(w, 500, fmt.Sprintf("Error creating user: %v", err))
+		return
+	}
+
+	jsonResponse(w, 200, databaseUserToUser(user))
+}
--- rss-project/main.go
+++ rss-project/main.go
@@ -55,6 +55,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", readinessHandler)
 	v1Router.Get("/err", errorHandler)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
@@ -63,7 +64,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %s", postString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error starting server:", err)
 	}
